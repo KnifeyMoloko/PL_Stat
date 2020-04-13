083787d9bef5aa53941df0d52b78dc1e45f8d5d6
KnifeyMoloko <maciej.a.cisowski@gmail.com>
2020-04-13 23:50:38 +0200

    further rework for conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,6 @@ from app.rest.rest_client import RestClient
 from app.helpers.environment import get_api_key
 
 
-def pytest_addoption(parser):
-    """
-    Add runtime options.
-    :param parser: pytest fixture for parsing runtime options
-    :return:
-    """
-    parser.addoption("--API_KEY", action="store", default="")
-
-
 # FIXTURES
 
 @pytest.fixture(scope="session")
